00cb90f852a137510cecce67f71cba6cc864098a
Alexey <tromoris@gmail.com>
2019-04-23 01:38:43 +0300

    Landing page frontend

--- src/main/resources/pages/MainPage.vue
+++ src/main/resources/pages/MainPage.vue
@@ -1,44 +1,100 @@
 <template>
     <v-container>
-        <v-layout row wrap >
-            <v-flex xs12 sm10 offset-sm1 class="course-item">
-                <v-card>
-                    <v-img src="http://www.stgeorges.co.uk/sites/default/files/Business%20English%20Courses%20London.png" aspect-ratio="2.75"></v-img>
-                    <v-card-title primary-title>
-                        <div>
-                            <h3 class="headline mb-0">English course</h3>
-                        </div>
-                    </v-card-title>
-                    <v-card-actions>
-                        <v-btn flat>more</v-btn>
-                    </v-card-actions>
-                </v-card>
+        <v-layout row wrap>
+            <v-flex xs12 sm6>
             </v-flex>
-            <v-flex xs12 sm10 offset-sm1 class="course-item">
-                <v-card>
-                    <v-img src="https://cdn.lynda.com/course/529631/529631-636531738331126522-16x9.jpg" aspect-ratio="2.75"></v-img>
-                    <v-card-title primary-title>
-                        <div>
-                            <h3 class="headline mb-0">Database course</h3>
+        </v-layout>
+        <v-layout row wrap>
+            <v-flex xs12>
+                <template>
+                  <v-card>
+                        <div class = "text">
+                        Courses on landing page:
                         </div>
-                    </v-card-title>
-                    <v-card-actions>
-                        <v-btn flat>more</v-btn>
-                    </v-card-actions>
-                </v-card>
+                  </v-card>
+                  <v-carousel>
+                    <v-carousel-item
+                      v-for="crs in courses"
+                      v-bind:src="crs.imageUrl"
+                      :key="crs.id">
+                      <div class = "name">
+                        {{crs.name}}
+                      </div>
+                      <div class = "descr">
+                      Description: {{crs.description}}
+                      </div>
+                    </v-carousel-item>
+                  </v-carousel>
+                  <v-card>
+                        <div class = "text">
+                        Trainers on landing page:
+                        </div>
+                  </v-card>
+                  <v-expansion-panel>
+                      <v-expansion-panel-content
+                        v-for="trainer in trainers"
+                        :key="trainer.id"
+                      >
+                        <template v-slot:header>
+                          <div>{{trainer.last_name}} {{trainer.first_name}}</div>
+                        </template>
+                        <v-card>
+                          <v-card-text>
+                            Here will be info about this trainer
+                          </v-card-text>
+                        </v-card>
+                      </v-expansion-panel-content>
+                    </v-expansion-panel>
+                </template>
             </v-flex>
         </v-layout>
     </v-container>
 </template>
 
 <script>
+
+    import axios from 'axios'
+
     export default {
         name: "MainPage",
+        data: function(){
+          return{
+              courses: [],
+              trainers: []
+          }
+        },
+        mounted() {
+            let self = this;
+            axios.get('http://localhost:8080/MainPage/courses-on-landing-page')
+                            .then(function (response) {
+                                self.courses = response.data;
+                            })
+                            .catch(function (error) {
+                                console.log(error);
+                            });
+            axios.get('http://localhost:8080/MainPage/trainers-on-landing-page')
+                            .then(function (response) {
+                                self.trainers = response.data;
+                            })
+                            .catch(function (error) {
+                                console.log(error);
+                            });
+
+},
+        methods: {
+            search(){
+
+            }
+        }
     }
+
 </script>
 
-<style scoped>
-    .course-item{
-        margin-bottom: 40px;
+<style>
+    .name{
+    }
+    .descr{
+    position:absolute;
+    bottom:50px;
     }
 </style>
