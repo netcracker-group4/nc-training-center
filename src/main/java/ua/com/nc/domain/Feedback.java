b79a24bae3989e296eb263572148c7aba267f7da
vital <vitaliy3.hryhorenko@gmail.com>
2019-05-03 23:23:23 +0300

    Add feedback domain and dao

---
+++ src/main/java/ua/com/nc/domain/Feedback.java
@@ -0,0 +1,29 @@
+package ua.com.nc.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import java.sql.Date;
+
+@Data
+@EqualsAndHashCode(of = {"id"})
+@AllArgsConstructor
+@ToString
+public class Feedback extends Entity<Integer> {
+    private Integer studentId;
+    private Integer trainerId;
+    private Integer courseId;
+    private String text;
+    private Date timeDate;
+
+    public Feedback(Integer id, Integer studentId, Integer trainerId, Integer courseId, String text, Date timeDate) {
+        super(id);
+        this.studentId = studentId;
+        this.trainerId = trainerId;
+        this.courseId = courseId;
+        this.text = text;
+        this.timeDate = timeDate;
+    }
+}
