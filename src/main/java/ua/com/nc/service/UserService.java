0923f9573e0720fcaf6ddb1be2dbfee2af9f60b4
Vladimir <laushkin048@gmail.com>
2019-05-07 19:34:41 +0300

    Fixed some bug

--- src/main/java/ua/com/nc/service/UserService.java
+++ src/main/java/ua/com/nc/service/UserService.java
@@ -20,7 +20,8 @@ public interface UserService extends UserDetailsService {
     void updateActive(User user);
 
     List<DtoTeacherAndManager> getAllManagers();
+
     List<DtoTeacherAndManager> getAllTrainers();
 
-    List<User> getAllTrainers();
+    //List<User> getAllTrainers();
 }
