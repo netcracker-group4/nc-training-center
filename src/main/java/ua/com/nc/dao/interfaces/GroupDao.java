669edf036909167513b27b0c9fe78e5d76e7ec9d
philip <lPhilip04@gmail.com>
2019-05-11 19:59:44 +0300

    Minor additional changes

--- src/main/java/ua/com/nc/dao/interfaces/GroupDao.java
+++ src/main/java/ua/com/nc/dao/interfaces/GroupDao.java
@@ -12,6 +12,8 @@ public interface GroupDao extends GenericDao<Group> {
 
     List<Group> getAllGroupsByStudent(int studentId);
 
+    Group getGroupById(Integer id);
+
     List<Group> getGroupByTrainerId(Integer id);
 
     void deleteUserFromGroup(String id, String userId);
